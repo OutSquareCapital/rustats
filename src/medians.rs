582be7a0ad944825e75ab719d53898f04256d636
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-06-25 17:04:32 +0200

    testing double loop optimization for median

--- src/medians.rs
+++ src/medians.rs
@@ -229,7 +229,7 @@ fn move_median_parallel<'py>(
     Ok(PyArray2::from_owned_array(py, output).into())
 }
 
-fn move_median_single<'py>(
+fn move_median_single_old<'py>(
     py: Python<'py>,
     array: PyReadonlyArray2<'py, f64>,
     length: usize,
@@ -251,6 +251,129 @@ fn move_median_single<'py>(
             for row in 0..num_rows {
                 let current_val: f64 = input_col[row];
 
+                window_q.push_back((current_val, row));
+
+                if !current_val.is_nan() {
+                    valid_count += 1;
+
+                    if let Some((max_small, _)) = small_heap.peek() {
+                        if current_val > max_small {
+                            large_heap.push(current_val, row);
+                        } else {
+                            small_heap.push(current_val, row);
+                        }
+                    } else {
+                        small_heap.push(current_val, row);
+                    }
+                }
+
+                if window_q.len() > length {
+                    let (old_val, old_idx) = window_q.pop_front().unwrap();
+
+                    if !old_val.is_nan() {
+                        valid_count -= 1;
+
+                        if small_heap.remove(old_idx) {
+                        } else {
+                            large_heap.remove(old_idx);
+                        }
+                    }
+                }
+                while small_heap.len() > large_heap.len() + 1 {
+                    if let Some((val, idx)) = small_heap.pop() {
+                        large_heap.push(val, idx);
+                    }
+                }
+
+                while large_heap.len() > small_heap.len() {
+                    if let Some((val, idx)) = large_heap.pop() {
+                        small_heap.push(val, idx);
+                    }
+                }
+                if valid_count >= min_length {
+                    if small_heap.len() > large_heap.len() {
+                        if let Some((val, _)) = small_heap.peek() {
+                            output_col[row] = val;
+                        }
+                    } else if !small_heap.is_empty() {
+                        let s_val: f64 = small_heap.peek().unwrap().0;
+                        let l_val: f64 = large_heap.peek().unwrap().0;
+                        output_col[row] = (s_val + l_val) / 2.0;
+                    }
+                }
+            }
+        }
+    });
+
+    Ok(PyArray2::from_owned_array(py, output).into())
+}
+
+fn move_median_single<'py>(
+    py: Python<'py>,
+    array: PyReadonlyArray2<'py, f64>,
+    length: usize,
+    min_length: usize
+) -> PyResult<Py<PyArray2<f64>>> {
+    let array = array.as_array();
+    let (num_rows, num_cols) = array.dim();
+    let mut output = Array2::<f64>::from_elem((num_rows, num_cols), f64::NAN);
+    let input_columns: Vec<_> = array.columns().into_iter().collect();
+    let mut output_columns: Vec<_> = output.columns_mut().into_iter().collect();
+
+    py.allow_threads(move || {
+        for (input_col, output_col) in input_columns.into_iter().zip(output_columns.iter_mut()) {
+            let mut small_heap = Indexed::new(length, num_rows, true);
+            let mut large_heap = Indexed::new(length, num_rows, false);
+            let mut window_q: VecDeque<(f64, usize)> = VecDeque::with_capacity(length + 1);
+            let mut valid_count: usize = 0;
+
+            for row in 0..length {
+                let current_val: f64 = input_col[row];
+
+                window_q.push_back((current_val, row));
+
+                if !current_val.is_nan() {
+                    valid_count += 1;
+
+                    if let Some((max_small, _)) = small_heap.peek() {
+                        if current_val > max_small {
+                            large_heap.push(current_val, row);
+                        } else {
+                            small_heap.push(current_val, row);
+                        }
+                    } else {
+                        small_heap.push(current_val, row);
+                    }
+                }
+
+                while small_heap.len() > large_heap.len() + 1 {
+                    if let Some((val, idx)) = small_heap.pop() {
+                        large_heap.push(val, idx);
+                    }
+                }
+
+                while large_heap.len() > small_heap.len() {
+                    if let Some((val, idx)) = large_heap.pop() {
+                        small_heap.push(val, idx);
+                    }
+                }
+                if valid_count >= min_length {
+                    if small_heap.len() > large_heap.len() {
+                        if let Some((val, _)) = small_heap.peek() {
+                            output_col[row] = val;
+                        }
+                    } else if !small_heap.is_empty() {
+                        let s_val: f64 = small_heap.peek().unwrap().0;
+                        let l_val: f64 = large_heap.peek().unwrap().0;
+                        output_col[row] = (s_val + l_val) / 2.0;
+                    }
+                }
+            }
+
+
+
+            for row in length..num_rows {
+                let current_val: f64 = input_col[row];
                 window_q.push_back((current_val, row));
 
                 if !current_val.is_nan() {
